8c90631cbb31d460c08192b0d624f975ef1893eb
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2022-11-22 14:42:51 +0800

    .

--- src/example/behavior_impl/config_behavior_impl.erl
+++ src/example/behavior_impl/config_behavior_impl.erl
@@ -1,6 +1,6 @@
 -module(config_behavior_impl).
 
--export([get_socket_handler_module/0, get_socket_package_module/0, get_socket_options_module/0]).
+-export([get_socket_handler_module/0, get_socket_package_module/0]).
 
 % 用于定义各种回调模块
 
@@ -10,6 +10,3 @@ get_socket_handler_module() ->
 %% 业务级别拆包
 get_socket_package_module() ->
   demo_server_socket_package_impl.
-
-get_socket_options_module() ->
-  demo_server_options_impl.
