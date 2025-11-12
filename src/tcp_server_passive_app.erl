44395844d6dcd731615a35f1b07c0735276f1e99
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-11-12 17:03:41 +0800

    .

--- src/tcp_server_passive_app.erl
+++ src/tcp_server_passive_app.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc tcp_server_passive public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(tcp_server_passive_app).
 
 -behaviour(application).
@@ -14,5 +9,3 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
-
-%% internal functions
